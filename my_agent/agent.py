938e934d404f0b56a0170090333f2ade0de5264c
kerazy11 <keiradenisemallare@gmail.com>
2025-10-29 16:27:33 +0100

    added code

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -21,5 +21,6 @@ root_agent = llm_agent.Agent(
     Make sure your answer is properly spaced and grammatically correct if a sentence answer is required. """,
     tools=[google_search, code_execution, web_fetch],
     instruction="You are a helpful agent that answers questions directly and concisely. You check if an answer can be found with information from the question itself, and if more information is needed, search online using google_search and use web_fetch to check the content of the pages. Make sure your answer is properly spaced and grammatically correct if a sentence answer is required. ",
+    tools=[web_search.web_search, google_search, web_fetch, code_execution],
     sub_agents=[],
 )

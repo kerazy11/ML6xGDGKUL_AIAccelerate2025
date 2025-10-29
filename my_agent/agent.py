78672460e12f98b95e42904ab17a7befc86db857
kerazy11 <keiradenisemallare@gmail.com>
2025-10-29 15:51:25 +0100

    update instruction

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -11,7 +11,7 @@ root_agent = llm_agent.Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description="A helpful assistant that can answer questions.",
-    instruction="You are a helpful agent that answers questions directly and concisely. You check if an answer can be found with information from the question itself, and if more information is needed, search online. Make sure your answer is properly spaced and grammatically correct if a sentence answer is required. ",
+    instruction="You are a helpful agent that answers questions directly and concisely. You check if an answer can be found with information from the question itself, and if more information is needed, search online using google_search and use web_fetch to check the content of the pages. Make sure your answer is properly spaced and grammatically correct if a sentence answer is required. ",
     tools=[web_search.web_search, google_search, code_execution, web_fetch],
     sub_agents=[],
 )

9385da74bf54feecc3a7b769d7aa233f8c84f1ec
Nabila3008 <74339458+Nabila3008@users.noreply.github.com>
2025-10-29 16:16:09 +0100

    without code_execution

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -5,13 +5,13 @@ The `root_agent` is used to evaluate your agent's performance.
 
 from google.adk.agents import llm_agent
 from my_agent.tools import web_search
-from google.adk.tools import google_search, code_execution, web_fetch
+from google.adk.tools import google_search, web_fetch
 
 root_agent = llm_agent.Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description="A helpful assistant that can answer questions.",
     instruction="You are a helpful agent that answers questions directly and concisely. You check if an answer can be found with information from the question itself, and if more information is needed, search online using google_search and use web_fetch to check the content of the pages. Make sure your answer is properly spaced and grammatically correct if a sentence answer is required. ",
-    tools=[web_search.web_search, google_search, code_execution, web_fetch],
+    tools=[web_search.web_search, google_search, web_fetch],
     sub_agents=[],
 )
